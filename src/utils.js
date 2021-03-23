35a59461b73e6bb4ca1313facbef42f0ccf70948
Kate <shepel.e@phystech.edu>
2021-03-24 00:01:32 +0300

    WIP

--- src/utils.js
+++ src/utils.js
@@ -7,3 +7,13 @@ const DEFAULT_POSTS = 8;
 const DEFAULT_IMAGES = [
   '/examples/photo-1588055312392-97068a233ee2.jpeg',
 ];
+
+function useEventListener(type, listener, options, target) {
+  if (target === undefined) target = window;
+  onMounted(() => {
+    target.addEventListener(type, listener, options);
+  });
+  onUnmounted(() => {
+    target.removeEventListener(type, listener, options);
+  });
+}
