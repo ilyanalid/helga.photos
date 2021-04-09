31432d0ab620e2a00fcd35c7d2116e4212170dcf
ilyanalid <lidzhigoriaeva.id@phystech.edu>
2021-04-10 00:05:40 +0300

    Add openDb func

--- src/utils.js
+++ src/utils.js
@@ -69,4 +69,30 @@ export function useWindowSize() {
       },
     };
 
-// tm
+
+    export function openDb() {
+        return new Promise((resolve) => {
+          const request = window.indexedDB.open(STORE_PREFIX, 1);
+      
+          request.onerror = function (event) {
+            alert(`Failed to open db:\n${event.toString()}`);
+          };
+      
+          request.onsuccess = function (event) {
+            resolve(request.result);
+          };
+      
+          request.onupgradeneeded = function (event) {
+            const db = event.target.result;
+            const stores = [];
+            for (let i = 0; i < 5; i++) {
+              if (!db.objectStoreNames.contains(`posts-${i}`))
+                stores.push(db.createObjectStore(`posts-${i}`, { keyPath: 'id' }));
+            }
+      
+            for (let i = 0; i < DEFAULT_POSTS; i++)
+              stores[0].put({ id: i, url: DEFAULT_IMAGES[i] || '' });
+          };
+        });
+      }
+      
