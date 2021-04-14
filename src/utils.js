24860826b383edf9ff7f84908b44e91ee37fb960
ilyanalid <lidzhigoriaeva.id@phystech.edu>
2021-04-15 00:06:19 +0300

    Add func resizedataUrl

--- src/utils.js
+++ src/utils.js
@@ -95,4 +95,42 @@ export function useWindowSize() {
           };
         });
       }
+    
+      export function resizedataURL(url, MAX_WIDTH = 512, MAX_HEIGHT = 512) {
+        const img = document.createElement('img');
       
+        return new Promise((resolve) => {
+          img.onload = function () {
+            // We create a canvas and get its context.
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+      
+            let width = img.width;
+            let height = img.height;
+      
+            if (width > height) {
+              if (width > MAX_WIDTH) {
+                height *= MAX_WIDTH / width;
+                width = MAX_WIDTH;
+              }
+            }
+            else {
+              if (height > MAX_HEIGHT) {
+                width *= MAX_HEIGHT / height;
+                height = MAX_HEIGHT;
+              }
+            }
+      
+            canvas.width = width;
+            canvas.height = height;
+      
+            ctx.drawImage(this, 0, 0, width, height);
+      
+            resolve(canvas.toDataURL());
+      
+            img.remove();
+          };
+      
+          img.src = url;
+        });
+      }
