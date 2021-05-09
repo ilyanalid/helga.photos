df3718f817cf446345ce81189a9b6fe818681338
ilyanalid <lidzhigoriaeva.id@phystech.edu>
2021-05-10 00:12:21 +0300

    wip

--- src/App.vue
+++ src/App.vue
@@ -449,18 +449,6 @@ a
   &.active
     opacity 1
 
-  &::before
-    content ''
-    position absolute
-    top 0
-    left 0
-    right 0
-    bottom 0
-    opacity 0.7
-    z-index -1
-    border-radius 0.3rem
-    background var(--theme-background)
-
   &::after
     content ''
     position absolute
